d4116778f4bd85ccf533c135b67735c59b46180d
Gabriel <gabitomereles@gmail.com>
2025-09-26 23:38:26 -0300

    entrega final

--- Desktop/Proyecto final-Gabriel Mereles/assets/js/app.js
+++ Desktop/Proyecto final-Gabriel Mereles/assets/js/app.js
@@ -1,4 +1,5 @@
 const lisProducts = document.querySelector('#listProducts');
+const contentProducts = document.querySelector('#contentProducts');
 
 let productsArray = [];
 
@@ -22,16 +23,24 @@ function selecData(prod) {
         img: prod.querySelector('img').src,
         title: prod.querySelector('h4').textContent,
         price: parseFloat(prod.querySelector('#currentPrice').textContent.replace('$','')),
-        id: parseInt(prod.querySelector('button[type="button"]').dataset.id, 10)
-
+        id: parseInt(prod.querySelector('button[type="button"]').dataset.id, 10),
+        quantity: 1
     }
 
-    productsArray = [...productsArray, productObj];
+    const exists = productsArray.some(prod => prod.id === productObj.id);
 
+    if (exists) {
+        showAlert('Ya añadido al carrito', 'error');
+        return;
+    }
+    
+    productsArray = [...productsArray, productObj];
+    showAlert('Añadido con éxito', 'succes');
     productsHtml();
 
 }
 function productsHtml() {
+    cleanHtml();
     productsArray.forEach(prod =>{
         const { img, title, price, quantity, id} = prod;
 
@@ -48,9 +57,51 @@ function productsHtml() {
         prodTitle.textContent = title;
         tdTitle.appendChild(prodTitle)
         
+        const tdPrice = document.createElement('td');
+        const prodPrice = document.createElement('p');
+        prodPrice.textContent = `$${price.toFixed(2)}`;
+        tdPrice.appendChild(prodPrice)
 
-        tr.append(tdImg, tdTitle);
+        const tdQuantity = document.createElement('td');
+        const prodQuantity = document.createElement('input');
+        prodQuantity.type = 'number';
+        prodQuantity.min = '1';
+        prodQuantity.value = quantity;
+        prodQuantity.dataset.id = id;
+        tdQuantity.appendChild(prodQuantity);
 
-        console.log(tr);
-    })
+        const tdDelete = document.createElement('td');
+        const prodDelete = document.createElement('button');
+        prodDelete.type = 'button';
+        prodDelete.textContent= 'X';
+        prodDelete.onclick = () => destroyProduct(id);
+        tdDelete.appendChild(prodDelete);
+
+        tr.append(tdImg, tdTitle, tdPrice, tdQuantity, tdDelete);
+
+        contentProducts.appendChild(tr);
+        
+    });
+}
+
+function destroyProduct(idProd) {
+    productsArray = productsArray.filter(prod => prod.id !== idProd);
+    showAlert('Producto Eliminado', 'success');
+    productsHtml
+}
+function cleanHtml() {
+    contentProducts.innerHTML = '';
+}
+
+
+function showAlert(message, type) {
+    const nonRepeatAlert = document.querySelector('.alert');
+    if (nonRepeatAlert) nonRepeatAlert.remove();
+    const div = document.createElement('div');
+    div.classList.add('alert', type);
+    div.textContent = message;
+
+    document.body.appendChild(div);
+
+    setTimeout(() => div.remove(), 5000); 
 }
